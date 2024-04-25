59bdca1f4cbf73c2133dbed6e931f65a71eb8f57
Jin Wei <seetohjinwei@gmail.com>
2024-04-25 15:02:02 +0800

    update main.py

--- json_parser/main.py
+++ json_parser/main.py
@@ -1,16 +1,36 @@
-import argparse
+from src.json_struct import parse
+from dataclasses import dataclass
+import sys
 
 
-def main() -> None:
-    pass
+@dataclass
+class Args:
+    input_file: str
+
+    @staticmethod
+    def get() -> "Args":
+        if len(sys.argv) < 2:
+            raise ValueError(f"""
+filepath is missing!
+Usage: `{sys.argv[0]} <filepath>`
+or `make run input=<filepath>`
+""")
+
+        input_file = sys.argv[1]
+        return Args(input_file=input_file)
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(
-        prog="JSON Parser",
-        description="Parses JSON into Python3 objects",
-    )
+def get_file_contents(path: str) -> str:
+    with open(path, "r") as f:
+        return f.read().strip()
+
+
+def main(args: Args) -> None:
+    txt = get_file_contents(args.input_file)
+    result = parse(txt)
+    print(result)
 
 
 if __name__ == "__main__":
-    main()
+    args = Args.get()
+    main(args)
