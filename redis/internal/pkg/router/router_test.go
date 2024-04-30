d11dc76518cbc95cc598dd94075b985f0deb7c55
Jin Wei <seetohjinwei@gmail.com>
2024-04-30 20:23:22 +0800

    fix tests

--- redis/internal/pkg/router/router_test.go
+++ redis/internal/pkg/router/router_test.go
@@ -3,20 +3,7 @@ package router
 import (
 	"reflect"
 	"testing"
-
-	"github.com/seetohjinwei/ccfyi/redis/pkg/messages"
 )
-
-func isError(message string) bool {
-	msg, err := messages.Deserialise(message)
-	if err != nil {
-		// not a valid message => not an error
-		return false
-	}
-
-	_, ok := msg.(*messages.Error)
-	return ok
-}
 
 func TestHandle(t *testing.T) {
 	tests := []struct {
@@ -34,12 +21,11 @@ func TestHandle(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			actual := r.handle(test.request)
-			hasError := isError(actual)
+			actual, ok := r.Handle(test.request)
 
-			if test.hasError && !hasError {
+			if test.hasError && ok {
 				t.Errorf("expected err, but succeeded with %+v", actual)
-			} else if !test.hasError && hasError {
+			} else if !test.hasError && !ok {
 				t.Errorf("expected no err, but got %+v", actual)
 			} else if !test.hasError && !reflect.DeepEqual(test.expected, actual) {
 				t.Errorf("expected %+v, but got %+v", test.expected, actual)
