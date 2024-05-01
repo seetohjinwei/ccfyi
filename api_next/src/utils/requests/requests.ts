1cfde14a7b7d01175f3798f2a71948073f1667df
Jin Wei <seetohjinwei@gmail.com>
2024-05-01 14:08:18 +0800

    some work on requests

--- api_next/src/utils/requests/requests.ts
+++ api_next/src/utils/requests/requests.ts
@@ -1,12 +1,31 @@
 import axios from "axios";
 
-/** TODO:
- * in an interface?
- * path params
- * query params
- * body
- */
+export interface RequestParameters {
+  path_params: PathParam[];
+  query_params: QueryParam[]; // need to perform url encoding
+  body: any; // need to stringify
+}
 
-export async function get<T>(url: string): Promise<T> {
+const empty_request_parameters: RequestParameters = {
+  path_params: [],
+  query_params: [],
+  body: null,
+};
+
+export interface PathParam {
+  key: string;
+  value: string;
+}
+
+export interface QueryParam {
+  key: string;
+  value: string | null;
+}
+
+export async function get<T>(
+  url: string,
+  params: RequestParameters = empty_request_parameters,
+): Promise<T> {
+  // TODO: do stuff with said params
   return await axios.get<T>(url).then((res) => res.data);
 }
