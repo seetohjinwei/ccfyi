4703e4a419e9f79a67f2baba49d0195bb51eb9ba
Jin Wei <seetohjinwei@gmail.com>
2024-05-18 12:38:53 +0800

    update build.zig to compile src/**/.zig

--- watcher/build.zig
+++ watcher/build.zig
@@ -3,7 +3,12 @@ const std = @import("std");
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -15,19 +20,26 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "watcher",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    const paths = try get_file_paths(allocator);
+    defer paths.deinit();
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    for (paths.items) |path| {
+        // std.debug.print("addStaticLibrary {s}\n", .{path});
+
+        const lib = b.addStaticLibrary(.{
+            .name = "watcher",
+            // In this case the main source file is merely a path, however, in more
+            // complicated build scripts, this could be a generated file.
+            .root_source_file = b.path(path),
+            .target = target,
+            .optimize = optimize,
+        });
+
+        // This declares intent for the library to be installed into the standard
+        // location when the user invokes the "install" step (the default step when
+        // running `zig build`).
+        b.installArtifact(lib);
+    }
 
     const exe = b.addExecutable(.{
         .name = "watcher",
@@ -64,15 +76,25 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
 
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    for (paths.items) |path| {
+        // std.debug.print("addTest {s}\n", .{path});
+
+        // Creates a step for unit testing. This only builds the test executable
+        // but does not run it.
+        const lib_unit_tests = b.addTest(.{
+            .root_source_file = b.path(path),
+            .target = target,
+            .optimize = optimize,
+        });
+
+        const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+        test_step.dependOn(&run_lib_unit_tests.step);
+    }
 
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
@@ -81,11 +103,25 @@ pub fn build(b: *std.Build) void {
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
+}
+
+fn get_file_paths(allocator: std.mem.Allocator) !std.ArrayList([]const u8) {
+    var dir = try std.fs.cwd().openDir("src", .{ .access_sub_paths = true, .iterate = true, .no_follow = false });
+    defer dir.close();
+    var walker = try dir.walk(allocator);
+    defer walker.deinit();
+
+    var paths = std.ArrayList([]const u8).init(allocator);
+
+    while (try walker.next()) |entry| {
+        if (entry.kind != std.fs.Dir.Entry.Kind.file or !std.ascii.endsWithIgnoreCase(entry.path, ".zig") or std.ascii.eqlIgnoreCase(entry.path, "main.zig")) {
+            continue;
+        }
+
+        const path = try std.mem.concat(allocator, u8, &[_][]const u8{ "src/", entry.path });
+        try paths.append(path);
+    }
+
+    return paths;
 }
