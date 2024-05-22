9b46a33bc62a8b8d9cf8e3299ca1d6a69863c32c
Jin Wei <seetohjinwei@gmail.com>
2024-05-22 20:22:34 +0800

    add hash

--- spellcheck/src/bloom_filter.zig
+++ spellcheck/src/bloom_filter.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const files = @import("./files.zig");
 
 const BloomFilter = struct {
+    allocator: std.mem.Allocator,
     m: u64,
     k: u64,
 
@@ -36,6 +37,18 @@ const BloomFilter = struct {
     }
 };
 
+fn hash(allocator: std.mem.Allocator, limit: u64, index: u64, data: []const u8) !u64 {
+    var key = [_]u8{0} ** 8;
+    std.mem.writeInt(u64, &key, index, std.builtin.Endian.big);
+
+    const buf = try allocator.alloc(u8, key.len + data.len);
+    @memcpy(buf[0..key.len], key[0..]);
+    @memcpy(buf[key.len..], data);
+    defer allocator.free(buf);
+
+    return std.hash.Fnv1a_64.hash(buf) % limit;
+}
+
 const p_default = 0.0001;
 
 /// Computes `m` and `k` from `n` and `p`..
@@ -52,10 +65,10 @@ fn get_m_k(n: u64, p: f64) struct { m: u64, k: u64 } {
 /// Creates a BloomFilter from a Reader.
 /// `approx_word_count` is an approximation of the number of words in the reader.
 /// To generate `approx_word_count`, you may use `files.approx_word_count`.
-pub fn from_reader(approx_word_count: u64, reader: std.io.AnyReader) BloomFilter {
+pub fn from_reader(allocator: std.mem.Allocator, approx_word_count: u64, reader: std.io.AnyReader) BloomFilter {
     const m_k = get_m_k(approx_word_count, p_default);
 
-    const bloom_filter = BloomFilter{ .m = m_k.m, .k = m_k.k };
+    const bloom_filter = BloomFilter{ .allocator = allocator, .m = m_k.m, .k = m_k.k };
 
     // TODO: does this only write the first line???
     reader.streamUntilDelimiter(bloom_filter.writer(), "\n", null);
@@ -81,4 +94,9 @@ test "get_m_k" {
         try std.testing.expectEqual(case.m, res.m);
         try std.testing.expectEqual(case.k, res.k);
     }
+}
+
+test "hash" {
+    try std.testing.expectEqual(65, try hash(std.testing.allocator, 100, 1, "key"));
+    try std.testing.expectEqual(36000358, try hash(std.testing.allocator, 42141512, 2, "key"));
 }
