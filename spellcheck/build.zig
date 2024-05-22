3cbfba054a6a7d95e3cf09b70a1bd2ba9b649dfd
Jin Wei <seetohjinwei@gmail.com>
2024-05-22 12:32:45 +0800

    init spellcheck

---
+++ spellcheck/build.zig
@@ -0,0 +1,127 @@
+const std = @import("std");
+
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
+    const paths = try get_file_paths(allocator);
+    defer paths.deinit();
+
+    for (paths.items) |path| {
+        // std.debug.print("addStaticLibrary {s}\n", .{path});
+
+        const lib = b.addStaticLibrary(.{
+            .name = "spellcheck",
+            // In this case the main source file is merely a path, however, in more
+            // complicated build scripts, this could be a generated file.
+            .root_source_file = b.path(path),
+            .target = target,
+            .optimize = optimize,
+        });
+
+        // This declares intent for the library to be installed into the standard
+        // location when the user invokes the "install" step (the default step when
+        // running `zig build`).
+        b.installArtifact(lib);
+    }
+
+    const exe = b.addExecutable(.{
+        .name = "spellcheck",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    b.installArtifact(exe);
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(exe);
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+
+    for (paths.items) |path| {
+        // std.debug.print("addTest {s}\n", .{path});
+
+        // Creates a step for unit testing. This only builds the test executable
+        // but does not run it.
+        const lib_unit_tests = b.addTest(.{
+            .root_source_file = b.path(path),
+            .target = target,
+            .optimize = optimize,
+        });
+
+        const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+        test_step.dependOn(&run_lib_unit_tests.step);
+    }
+
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
+
+fn get_file_paths(allocator: std.mem.Allocator) !std.ArrayList([]const u8) {
+    var dir = try std.fs.cwd().openDir("src", .{ .access_sub_paths = true, .iterate = true, .no_follow = false });
+    defer dir.close();
+    var walker = try dir.walk(allocator);
+    defer walker.deinit();
+
+    var paths = std.ArrayList([]const u8).init(allocator);
+
+    while (try walker.next()) |entry| {
+        if (entry.kind != std.fs.Dir.Entry.Kind.file or !std.ascii.endsWithIgnoreCase(entry.path, ".zig") or std.ascii.eqlIgnoreCase(entry.path, "main.zig")) {
+            continue;
+        }
+
+        const path = try std.mem.concat(allocator, u8, &[_][]const u8{ "src/", entry.path });
+        try paths.append(path);
+    }
+
+    return paths;
+}
