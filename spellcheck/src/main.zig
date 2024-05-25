7231695453b149d727665169dfea814e16217512
Jin Wei <seetohjinwei@gmail.com>
2024-05-25 20:07:56 +0800

    add args.zig

--- spellcheck/src/main.zig
+++ spellcheck/src/main.zig
@@ -1,26 +1,18 @@
 const std = @import("std");
+const args = @import("./args.zig");
 
 pub fn main() !void {
-    // TODO: https://ziglang.org/documentation/master/#toc-Choosing-an-Allocator
+    // this is a CLI program :)
+    // https://ziglang.org/documentation/master/#toc-Choosing-an-Allocator
 
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const allocator = arena.allocator();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const arguments = args.parse(allocator) catch {
+        std.process.exit(1);
+    };
 
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    std.debug.print("{s}\n", .{arguments.program_name});
 }
